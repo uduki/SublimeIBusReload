0a9608c6984541e79480a644a949a073fddc259e
uduki <cat@oborogin.org>
2013-05-20 18:31:48 +0900

    fix calling method name

--- SublimeIBusReload.py
+++ SublimeIBusReload.py
@@ -1,9 +1,10 @@
 import os
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 
 class SublimeIbusReloadCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		pkgDir = sublime.packagesPath()
+		pkgDir = sublime.packages_path()
 		target = "sublimeibusplugin.py"
 		cmd = "touch"
 		os.system(cmd + " " + pkgDir + "/" + target)
