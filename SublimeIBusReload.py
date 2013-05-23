94cb15bfb4f60c9b836895ff015d5eab6d4b0b92
uduki <cat@oborogin.org>
2013-05-23 16:21:12 +0900

    fix target file path

--- SublimeIBusReload.py
+++ SublimeIBusReload.py
@@ -5,6 +5,6 @@ import sublime_plugin
 class SublimeIbusReloadCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		pkgDir = sublime.packages_path()
-		target = "sublimeibusplugin.py"
+		target = "SublimeIBus/sublimeibusplugin.py"
 		cmd = "touch"
 		os.system(cmd + " " + pkgDir + "/" + target)
